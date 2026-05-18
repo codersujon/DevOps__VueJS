30112ed413dcc0141995fc6711c876c479c70559
MD SUJAN AHMED <csesujon155@gmail.com>
2026-05-19 01:22:56 +0600

    Ready to Deploy DevOps plus VueJs Project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+import viteLogo from '../assets/vite.svg'
+import heroImg from '../assets/hero.png'
+import vueLogo from '../assets/vue.svg'
+
+const count = ref(0)
+</script>
+
+<template>
+  <section id="center">
+    <div class="hero">
+      <img :src="heroImg" class="base" width="170" height="179" alt="" />
+      <img :src="vueLogo" class="framework" alt="Vue logo" />
+      <img :src="viteLogo" class="vite" alt="Vite logo" />
+    </div>
+    <div>
+      <h1>Cloud Experts</h1>
+      <p>Welcome to <code>cloudexperts.net</code> and save to test <code>HMR</code></p>
+    </div>
+    <button type="button" class="counter" @click="count++">
+      Count is {{ count }}
+    </button>
+  </section>
+
+  <div class="ticks"></div>
+
+  <div class="ticks"></div>
+  <section id="spacer"></section>
+</template>
